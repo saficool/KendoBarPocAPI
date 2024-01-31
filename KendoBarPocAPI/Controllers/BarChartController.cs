eabeeda0b99d9b875df7729309b7c4d395fc7717
Safikul Islam <islam.safikul@hotmail.com>
2024-01-31 11:11:19 +0530

    Add project files.

---
+++ KendoBarPocAPI/Controllers/BarChartController.cs
@@ -0,0 +1,45 @@
+﻿using KendoBarPocAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace KendoBarPocAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BarChartController : ControllerBase
+    {
+        string chartData = @"
+        {
+            ""series"":[               
+                {
+                    ""data"":[
+                        {""x"":""P1"", ""value"":""128.14""},
+                        {""x"":""P2"", ""value"":""112.61""},
+                        {""x"":""P3"", ""value"":""163.21""},
+                        {""x"":""P4"", ""value"":""229.98""},
+                        {""x"":""P5"", ""value"":""90.54""}
+                    ]
+                },
+                {
+                    ""data"":[
+                        {""x"":""P1"", ""value"":""90.54""},
+                        {""x"":""P2"", ""value"":""104.19""},
+                        {""x"":""P3"", ""value"":""150.67""},
+                        {""x"":""P4"", ""value"":""120.43""},
+                        {""x"":""P5"", ""value"":""200.34""}
+                    ]
+                }               
+            ]
+        }";
+        public BarChartController() { }
+
+        [HttpGet]
+        [Route("GetBarChartData")]
+        public IActionResult GetBarChartData()
+        {
+            var chartDataJson = JsonConvert.DeserializeObject<Root>(chartData);
+            return Ok(chartDataJson);
+        }
+    }
+}
---
+++ KendoBarPocAPI/Models/Chart.cs
@@ -0,0 +1,19 @@
+﻿namespace KendoBarPocAPI.Models
+{
+    public class Root
+    {
+        public List<DataItemContainer>? Series { get; set; }
+    }
+
+    public class DataItemContainer
+    {
+        public List<DataItem>? Data { get; set; }
+    }
+
+    public class DataItem
+    {
+        public string? X { get; set; }
+        public string? Value { get; set; }
+    }
+
+}
